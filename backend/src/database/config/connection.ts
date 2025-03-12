f7d043879b57b6ba11de0d4de219137d9a5064bf
Lucas Rodrigues <lucasrodriguesbezerra@outlook.com>
2025-03-12 18:06:09 -0300

    Adicionei no .env um STAGE para diferenciar DEV de PROD no banco

--- backend/src/database/config/connection.ts
+++ backend/src/database/config/connection.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { join } from "path";
 import { DataSource } from "typeorm";
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, STAGE } = process.env;
 
 export const connection = new DataSource({
     type: "postgres",
@@ -11,7 +11,7 @@ export const connection = new DataSource({
     username: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
-    synchronize: true,
+    synchronize: STAGE === "development" ? true : false,
     logging: true,
     entities: [join(__dirname , "../entities/**/*.{ts,js}")],
     migrations: [join(__dirname,  "../migrations/**/*.{ts,js}")],
