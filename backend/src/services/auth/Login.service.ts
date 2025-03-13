d298629659d8a8184066f28a627e4f43fb56abc1
Lucas Rodrigues <lucasrodriguesbezerra@outlook.com>
2025-03-13 13:24:35 -0300

    adicionei a lib bcrypt para hash das senhas

--- backend/src/services/auth/Login.service.ts
+++ backend/src/services/auth/Login.service.ts
@@ -1,9 +1,10 @@
 import { LoginSchema } from "../../schemas/LoginSchema";
 import { UserSchema } from "../../schemas/UserSchema"
+import { comparePasswords } from "../../utils/password";
 
 class LoginService {
     async execute(login: LoginSchema, user: UserSchema) {
-        if(login.password === user.password) {
+        if(await comparePasswords(login.password,user.password)) {
             return user;
         } else {
             return false;
--- backend/src/services/auth/Register.service.ts
+++ backend/src/services/auth/Register.service.ts
@@ -1,9 +1,13 @@
 import { UserRepository } from "../../database/repositories/UserRepository";
 import { UserSchema } from "../../schemas/UserSchema";
+import { hash } from "../../utils/password";
 
 class RegisterService {
     async execute(body: UserSchema) {
-        const register = UserRepository.create(body);
+        const register = UserRepository.create({
+                    ...body,
+                    password: await hash(body.password)
+                });
         const user = await UserRepository.save(register);
         return user;
     }
